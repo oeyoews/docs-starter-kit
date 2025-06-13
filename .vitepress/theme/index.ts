1a7e8a6673fb1e4f4c5c064fa90347a72d060b4b
oeyoews <jyao4783@gmail.com>
2025-06-13 16:17:48 +0800

    feat: support bprogress

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,6 +4,8 @@ import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import './style.css';
 import 'virtual:uno.css';
+import '@bprogress/core/css';
+import { BProgress } from '@bprogress/core';
 
 export default {
   extends: DefaultTheme,
@@ -13,6 +15,15 @@ export default {
     });
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
+    // const inBrowser = typeof window !== 'undefined';
+    // if (inBrowser) {
+    BProgress.configure({ showSpinner: false });
+    router.onBeforeRouteChange = () => {
+      BProgress.start(); // 开始进度条
+    };
+    router.onAfterRouteChange = () => {
+      BProgress.done(); // 停止进度条
+    };
+    // }
   },
 } satisfies Theme;
